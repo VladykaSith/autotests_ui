8ed64f837f0bed0e2ca53352a789767c34ea777d
Наумов Владислав Владимирович <VNaumov@aplana-it.ru>
2026-08-26 15:55:12 +0700

    Добавление Allure шагов в автотесты

--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page
-
+import allure
 from components.base_component import BaseComponent
 from elements.button import Button
 from elements.text import Text
@@ -14,6 +14,7 @@ class CreateCourseToolbarViewComponent(BaseComponent):
             page, 'create-course-toolbar-create-course-button', 'Create course'
         )
 
+    @allure.step("Check visible create course toolbar view")
     def check_visible(self):
         self.title.check_visible()
         self.title.check_have_text('Create course')
