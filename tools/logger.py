aee2518727d443634e48057ea487943aba26eaee
Наумов Владислав Владимирович <VNaumov@aplana-it.ru>
2026-09-02 15:25:09 +0700

    Логирование взаимодействий со страницей

---
+++ tools/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    handler=logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
+
+
+
+logger = get_logger("PAGE")
+logger.info('Hello INFO!')
+logger.warning('Hello WARNING!')
+logger.error('Hello ERROR!')
+logger.critical('Hello! CRITICAL!')
--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -1,6 +1,4 @@
 from playwright.sync_api import Page, Route
 
-
-
 def mock_static_resources(page: Page):
     page.route("**/*.{ico,png,jpg,svg,webp,mp3,mp4,woff,woff2}", lambda route: (print(f'\n\naborting: {route.request.url}'), route.abort()))
