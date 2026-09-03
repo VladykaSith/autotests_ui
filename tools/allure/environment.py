dade95955369fd78e2eb9a2b72fffa46e4b3bfd3
Наумов Владислав Владимирович <VNaumov@aplana-it.ru>
2026-09-03 11:07:15 +0700

    minor

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,8 +1,13 @@
 from config import settings
+import platform
+import sys
 
 def create_allure_environment_file():
-    items = [f'{key}={val}'for key, val in settings.model_dump().items()]
-    properties='\n'.join(items)
+    items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    items.append(f'os_info={platform.system()}, {platform.release()}')
+    items.append(f'python_version={sys.version}')
+
+    properties = '\n'.join(items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
         file.write(properties)
